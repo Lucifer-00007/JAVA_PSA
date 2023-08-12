961081c7d8623a3a9bc87dab91d5650c51f1f1db
LUCIEFR <ani.mimansatech@gmail.com>
2023-08-12 23:24:50 +0530

    up

--- eclipse-workspace/app_java_1/src/app_java_1/A.java
+++ eclipse-workspace/app_java_1/src/app_java_1/A.java
@@ -1,7 +1,21 @@
 package app_java_1;
 
 public class A {
-public static void main(String[] args) {
-	System.out.print(false);
-}
+	A(int x) { // no of args=1, type=int
+		System.out.println(x);
+	}
+
+	A(char x) { // no of args=1, type=char
+		System.out.println(x);
+	}
+
+	A(double x) { // no of args=1, type=double
+		System.out.println(x);
+	}
+
+	public static void main(String args[]) {
+		A a1 = new A(10);
+		A a2 = new A('t');
+		A a3 = new A(15.5);
+	}
 }
